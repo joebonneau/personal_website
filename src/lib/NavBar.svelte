f035d7790137d41f57981e6ae9897533a4870519
Joe Bonneau <joeboneau@gmail.com>
2023-06-16 12:23:41 -0700

    progress

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  // import sticky from "./sticky.js"
+  import User from "./user-solid.svelte"
+  import NavLink from "./NavLink.svelte";
+
+  // export let stickToTop = true;
+  // let isStuck = false;
+
+  // function handleStuck(e: CustomEvent) {
+  //   isStuck = e.detail.isStuck;
+  // }
+
+  const menuOptions = [
+    ["Home", "/"],
+    ["About", "/about"],
+    ["Experience", "/experience"],
+    ["Blog", "/blog"],
+  ];
+</script>
+
+<div 
+  class="navbar-container"
+>
+  <div class="left-nav">
+    <!-- <User /> -->
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+  </div>
+  <div class="right-nav">
+    {#each menuOptions as [name, route]}
+      <NavLink path={route} text={name} />
+    {/each}
+  </div>
+</div>
+
+<style lang="scss">
+  div.navbar-container {
+    display: flex;
+    position: sticky;
+    // padding: 0.5rem;
+    // padding-top: 10px;
+    background-color: #415D43;
+
+    .left-nav {
+      display: flex;
+      flex: 1;
+      justify-content: start;
+    }
+
+    .right-nav {
+      display: flex;
+      flex: 1;
+      justify-content: end;
+    }
+  }
+
+</style>
---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { goto } from "$app/navigation"
+  export let path: string;
+  export let text: string;
+</script>
+
+<div>
+  <button on:click={() => goto(path)}>
+    {text}
+  </button>
+</div>
+
+<style lang="scss">
+  div {
+    background-color: inherit;
+  }
+  button {
+    border: none;
+    color: white;
+    font-weight: bold;
+    font-size: 1.25rem;
+    padding: 0.5rem 1rem;
+    background-color: inherit;
+    // background-color: #709775; 
+  }
+
+  button:hover {
+    background-color: #709775;
+    transition: background-color 0.15s ease-in-out;
+    cursor: pointer;
+  }
+</style>
